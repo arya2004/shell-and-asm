57f200d5af43c43b811c3319324d79ce5be5cd69
arya <arya20j4@gmail.com>
2023-11-19 21:20:58 +0530

    ft: basic

---
+++ Revi/c.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VAR="lol"
+
+echo 'hello, $VAR'
+echo "hello, $VAR"
+echo "hello, $VAR \$VAR"
+
+# '\' ignores special char meaning
+
+# backtich or dollor parenthesis
+
+TIM=`uptime`
+echo $TIM
+
+LEL=`who`
+echo $LEL
